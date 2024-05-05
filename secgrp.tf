275ab489d45dd362172307c7792130304c930ca7
Mithilesh kumar <mithileshkumar233228@gmail.com>
2024-05-05 10:11:44 +0530

    Backend Services

--- secgrp.tf
+++ secgrp.tf
@@ -1,36 +1,36 @@
 resource "aws_security_group" "vprofile-bean-elb-sg" {
-  name = "vprofile-bean-elb-sg"
+  name        = "vprofile-bean-elb-sg"
   description = "Security group for bean-elb"
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "vprofile-bastion-sg" {
-  name = "vprofile-bastion-sg"
+  name        = "vprofile-bastion-sg"
   description = "Security group for bastionisioner ec2 instance"
   vpc_id      = module.vpc.vpc_id
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = [var.MYIP]
   }
 }
@@ -71,11 +71,11 @@ resource "aws_security_group" "vprofile-backend-sg" {
   }
 }
 
- resource "aws_security_group_rule" "sec_group_allow_itself" {
-   type = "ingress"
-   from_port = 0
-   to_port = 65535
-   protocol = "tcp"
-   security_group_id = aws_security_group.vprofile-backend-sg.id
-   source_security_group_id = aws_security_group.vprofile-backend-sg.id
- }
+resource "aws_security_group_rule" "sec_group_allow_itself" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vprofile-backend-sg.id
+  source_security_group_id = aws_security_group.vprofile-backend-sg.id
+}
